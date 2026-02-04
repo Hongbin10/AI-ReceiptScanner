dc9009e2dd6e521639c0a9d44847cabf64a78c99
Hongbin10 <jdmjdm1998@163.com>
2026-02-04 00:30:22 +0000

    12. edit server.js improt dontenv connenctDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 
-
+const port = process.env.PORT || 5001;
 
 
 app.listen(5001, () => {
   console.log('Server is running on port 5001');
+});
+
+connectDB().then(() => {
+  app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 });
